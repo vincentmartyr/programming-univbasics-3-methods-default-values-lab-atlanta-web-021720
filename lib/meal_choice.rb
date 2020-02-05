5214ac7efb795c96afa69e021618fa9bec0032f8
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-05 00:29:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,4 +5,6 @@ def meal_choice(veg1, veg2, protein = 'meat')
  puts "A plate of #{protein} with #{veg1} and #{veg2}."
  end
 
-return meal_choice ("broccoli", "macaroni", "tofu")
+meal_choice ("broccoli", "macaroni", "tofu")
+
+meal_choice ("broccoli", "macaroni")
